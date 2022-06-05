7470062d6a47505cb9c95566cbb8f0dda2af331e
AESalva <eduardo_salva@hotmail.com>
2022-06-05 13:49:44 -0300

    objetos

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ console.log(nombre);
 let auto = 'Fiat Argo';
 console.log(auto);
 
+let persona = {nombre:'Eduardo' , edad: 35 , apellido: 'Salva'}
+console.log(persona.edad);
+
+console.log(persona.apellido);
+
+console.log(persona.nombre);
+
 const indumentaria = ['camisa', 'pantalon', 'zapatillas','medias','campera'];
 let vestimenta = [indumentaria];
 console.log(indumentaria);
