df2acd70b01895e013f916908bc224ca930dae51
AESalva <eduardo_salva@hotmail.com>
2022-06-22 23:31:35 -0300

    form inputs

--- js_problemas/index.js
+++ js_problemas/index.js
@@ -3,7 +3,7 @@ let titulo = document.createElement('h1');
 titulo.innerHTML = ('Hola como estas');
 document.body.append(titulo);
 
-let main = document.createElement('div');
+let main = document.createElement('main');
 
 let parrafo = document.createElement('p');
 parrafo.innerHTML = 'LOREM  PICSUM';
@@ -12,4 +12,27 @@ main.append(parrafo);
 document.body.append(main);
 
 
+let form = document.createElement('div');
+let formInput = document.createElement('input'); 
+let button = document.createElement('button');
+
+
+
+formInput.type = 'text';
+formInput.setAttribute('name','nombre');                // set reemplaza , pisa, saca el valor viejo
+formInput.setAttribute('placeholder','Nombre');
+
+button.innerHTML = 'enviar';
+
+
+
+button.addEventListener('click', function(e) {
+    let textInput = document.createElement('p');
+    textInput.innerHTML = formInput.value;
+    document.body.append(textInput)});
+
+form.append(formInput);
+form.append(button);
+main.append(form);
+ 
 
