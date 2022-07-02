ad3c0fabc03c97adc0cecfad3377451187678f9a
AESalva <eduardo_salva@hotmail.com>
2022-07-02 20:09:58 -0300

    practice

--- 09_class/script.js
+++ 09_class/script.js
@@ -1,3 +1,45 @@
 let texto = document.getElementById('text');
 
 texto.innerHTML = 'Nuevo texto random';
+
+
+
+let divNuevo = document.createElement('div');
+divNuevo.innerHTML = 'texto del div';
+
+let mainCreado = document.getElementsByTagName('main')[0];
+mainCreado.append(divNuevo);
+
+
+mainCreado.classList.add('mainCreadoStyle');
+
+divNuevo.classList.add('divNuevoStyle');
+
+
+// modularizacion
+
+let inputTitulo = document.getElementById('inpText');
+let inputImagenURL = document.getElementById('inpImagenURL');
+let btnAgregar = document.getElementById('inpButton');
+
+
+function addProducto(titulo, imagenURL){
+                                              // tomar los valores de los inputs
+                                              // creacion de los elementos 
+                                              // agregado de los valores
+    imagenURL.setAttribute('src', imagenURL)
+
+
+}
+
+
+
+
+
+function error(){
+    inputTitulo.value.length < 4 ? 
+    inputTitulo.classList.add('error'):
+    inputTitulo.classList.remove('error');
+
+}
+
