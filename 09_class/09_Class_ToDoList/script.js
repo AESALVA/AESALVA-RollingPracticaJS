92ac017caf30977139b1aec3003f9bb64f643385
AESalva <eduardo_salva@hotmail.com>
2022-07-05 20:46:00 -0300

    changes

--- 09_class/09_Class_ToDoList/script.js
+++ 09_class/09_Class_ToDoList/script.js
@@ -1,10 +1,12 @@
 let inputTarea = document.getElementById("tarea");
 let lista = document.getElementById("tareas");
 let btnAgregar = document.getElementById("btnAddTarea");
+
 // let tareas = JSON.parse(localStorage.getItem("tareas"));
 // console.log(tareas);
 
 btnAgregar.addEventListener("click", agregarTarea);
+
 // cargaInicial();
 
 // function cargaInicial() {
@@ -21,12 +23,19 @@ btnAgregar.addEventListener("click", agregarTarea);
 function agregarTarea() {
   let liNuevo = document.createElement("li");
   liNuevo.addEventListener("click", (event) => {
-    event.target.remove();
-  });
+     event.target.remove();
+   });    
   liNuevo.innerHTML = inputTarea.value;
   lista.append(liNuevo);
+ 
 //   tareas.push(inputTarea.value);
 //   localStorage.setItem("tareas", JSON.stringify(tareas));
   inputTarea.value = "";
   inputTarea.focus();
 }
+
+
+
+
+
+
