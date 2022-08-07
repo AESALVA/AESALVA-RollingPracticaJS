534ee44d3a65cb2661bae45d819f93896cc56229
AESalva <eduardo_salva@hotmail.com>
2022-08-07 18:17:24 -0300

    changes

--- 12_Class/rcs-3i-js-crud/scripts/script.js
+++ 12_Class/rcs-3i-js-crud/scripts/script.js
@@ -7,7 +7,7 @@ const products =
 
 const newArticleButton = document
   .getElementById('newArticleButton')
-  .addEventListener('click', () => newArticle());
+  .addEventListener('click',() => newArticle());
 
 const newArticleId =
   document.getElementById('id');
@@ -19,6 +19,18 @@ const newArticleModalMessage =
   document.getElementById(
     'newArticleModalMessage'
   );
+
+// elementos HTML para modificar
+
+const newIdModify =
+document.getElementById('idToModify');
+const newNameModify =
+document.getElementById('nameToModify');
+const newPriceModify =
+document.getElementById('priceToModify');
+const ModifyButton = document.getElementById('modifyButton').addEventListener('click', ()=>upDateNewResults());
+
+
 
 const results =
   document.getElementById('results');
@@ -62,6 +74,7 @@ const updateResults = () => {
     p.name
       .toUpperCase()
       .includes(input.toUpperCase())
+      
   );
 
   // Recorro el array de productos para inyectar codigo HTML tantas veces como productos contenga
@@ -75,12 +88,16 @@ const updateResults = () => {
     card.innerHTML = `<div class="d-flex justify-content-between align-items-center">
     <div>#${p.id} ${p.name} ($${p.price})</div>
     <div>
-      <button
+      <button 
         type="button"
-        class="btn btn-warning rounded-0"
-      >
+        class="btn btn-warning rounded-0" id="${p.name}"
+        data-bs-toggle="modal"
+          data-bs-target="#modifyModal">
+
         <i class="fa-solid fa-pen text-white"></i>
-      </button>
+        </button>
+
+      
       <button
         type="button"
         class="btn btn-danger rounded-0" id="${p.id}"
@@ -90,19 +107,18 @@ const updateResults = () => {
     </div>
   </div>`;
     results.appendChild(card);
-    const button = document
+    const delbutton = document
       .getElementById(p.id)
       .addEventListener('click', () =>
         delArticle(p)
       );
+    const modbutton = document.getElementById(p.name).addEventListener('click', ()=>findArticle(p)); 
   });
 };
 
 // Funcion para borrar productos (Sale mal)
 const delArticle = product => {
-  console.log(product);
   const index = products.indexOf(product);
-  console.log(index);
   products.splice(index, 1);
   localStorage.setItem(
     'products',
@@ -111,4 +127,48 @@ const delArticle = product => {
   updateResults();
 };
 
+const findArticle = product => {
+const index = products.indexOf(product);
+
+newIdModify.value = product.id;
+newNameModify.value = product.name;
+newPriceModify.value = product.price;
+
+
+
+
+}
+
+
+
+
+ const upDateNewResults = () => {
+
+
+  products[0].id = newIdModify.value;
+  updateResults();
+ }
+
+
+
+
+
+
+
+
 updateResults();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
