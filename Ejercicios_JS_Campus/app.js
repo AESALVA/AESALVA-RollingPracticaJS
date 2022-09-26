6fea0c417bfdeb10e12303aa3ef87cec8a8dc165
AESalva <eduardo_salva@hotmail.com>
2022-09-25 23:56:13 -0300

    ejercicio 11 solved

--- Ejercicios_JS_Campus/app.js
+++ Ejercicios_JS_Campus/app.js
@@ -294,3 +294,44 @@
     
 //     document.write("<br>");
 // }
+
+
+// let filas = parseInt(prompt('elija nro de filas'));
+// let columnas = parseInt(prompt('elija el nro de columnas'));
+
+
+// let total = filas * columnas;
+
+// document.write('<table border>');
+// for(let indiceFila=0; indiceFila < filas; indiceFila++){
+//     document.write('<tr>');
+
+//     for(let indiceColumna=0; indiceColumna< columnas; indiceColumna++){
+//         document.write('<td>'+total+'</td>');
+//          total--; 
+//     }
+//     document.write('</tr>');
+// }
+// document.write('</table>');
+
+// let nombre1 = prompt('primer nombre:');
+// let edad1 = parseInt(prompt('primer nro de edad'));
+
+// let nombre2 = prompt('segundo nombre:');
+// let edad2 = parseInt(prompt('segundo nro de edad'));
+
+// let nombre3 = prompt('tercer nombre');
+// let edad3 = parseInt(prompt('tercer nro de edad'));
+
+// let mayor = Math.max(edad1,edad2,edad3);
+
+// if (mayor === edad1) {
+//     document.write(`el mayor es ${nombre1}`)
+// } else if(mayor === edad2){
+//     document.write(`el mayor es ${nombre2}`)
+// }else{
+//     document.write(`el mayor es ${nombre3}`)
+// }
+
+
+
